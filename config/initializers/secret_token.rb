c24f60c1e8fb25640b29016d8b255c30d85b611a
Paul Dyck <pauldyck@deb.fulliautomatix.com>
2011-03-01 13:33:15 -0600

    Initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Book5::Application.config.secret_token = '1c822cc7e1971bf216a9b5e8ffcc757d336786460725494b485c324c482fbe140aebcde3aad5e72dd962d0a9b6ad1d3c348fd953bb2c92d5b6b6f1765852ea30'
